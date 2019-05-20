a2a2fcb652f46d95e76c75601fb3db0a52f73d85
Jason Boyd <jason.boyd.ce@gmail.com>
2019-05-19 19:53:43 -0700

    Let there be light

---
+++ Cmdlets/ExportDacPac.psm1
@@ -0,0 +1,51 @@
+function Export-DacPac {
+    [CmdletBinding()]
+    param (
+        [Parameter(ParameterSetName='NotConnectionString')]
+        [string]
+        $Server,
+
+        [Parameter(Mandatory = $true,ParameterSetName='NotConnectionString')]
+        [string]
+        $DatabaseName,
+
+        [Parameter(ParameterSetName='NotConnectionString')]
+        [PSCredential]
+        $Credential,
+
+        [Parameter(ParameterSetName='ConnectionString')]
+        [string]
+        $ConnectionString,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Destination,
+
+        [switch]
+        $Overwrite
+    )
+
+    if ((Test-Path $Destination) -and !$Overwrite) {
+        Write-Error "File already exists."
+    }
+
+    $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
+
+    if ($PSBoundParameters.ParameterSetName -eq 'ConnectionString') {
+        $connectionStringBuilder.ConnectionString = $connectionString
+    }
+    else {
+        if ([string]::IsNullOrWhiteSpace($Server)) {
+            $Server = 'localhost'
+        }
+        
+        $connectionStringBuilder.Add("Data Source", $Server)
+        $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
+        $connectionStringBuilder.Add("User Id", $Credential.UserName)
+        $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
+    }
+
+    $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
+    $service.Extract($Destination, $DatabaseName, "SqlDevOps", [Version]::new(1,0,0,0))
+    Get-Item -Path $Destination
+}
