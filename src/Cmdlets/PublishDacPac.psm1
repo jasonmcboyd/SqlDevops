548bc720d3e31f2cc2437350f9f40787b5605328
Jason Boyd <jason.boyd.ce@gmail.com>
2019-05-19 19:53:43 -0700

    Let there be light

---
+++ src/Cmdlets/PublishDacPac.psm1
@@ -0,0 +1,48 @@
+function Publish-DacPac {
+    [CmdletBinding()]
+    param (
+        [string]
+        $Path,
+
+        [Parameter(ParameterSetName='NotConnectionString')]
+        [string]
+        $Server,
+
+        [Parameter(Mandatory = $true,ParameterSetName='NotConnectionString')]
+        [string]
+        $DatabaseName,
+
+        [Parameter(ParameterSetName='NotConnectionString')]
+        [PSCredential]
+        $Credential,
+
+        [Parameter(ParameterSetName='ConnectionString')]
+        [string]
+        $ConnectionString
+    )
+
+    if (!(Test-Path $Path)) {
+        throw "DacPac not found at: $Path"
+    }
+
+    $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
+
+    if ($PSBoundParameters.ParameterSetName -eq 'ConnectionString') {
+        $connectionStringBuilder.ConnectionString = $connectionString
+    }
+    else {
+        if ([string]::IsNullOrWhiteSpace($Server)) {
+            $Server = 'localhost'
+        }
+        
+        $connectionStringBuilder.Add("Data Source", $Server)
+        $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
+        $connectionStringBuilder.Add("User Id", $Credential.UserName)
+        $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
+    }
+
+    $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
+    $package = [Microsoft.SqlServer.Dac.DacPackage]::Load($Path)
+    $options = [Microsoft.SqlServer.Dac.PublishOptions]::new()
+    $service.Publish($package, $DatabaseName, $options)
+}
