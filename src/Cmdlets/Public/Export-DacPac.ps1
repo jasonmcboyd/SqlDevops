5a8da04db3c06d52728195a17845ee9b52bc2e1e
Jason Boyd <jason.boyd.ce@gmail.com>
2019-06-04 09:34:01 -0700

    Initial implementation of pipeline support

--- src/Cmdlets/Public/Export-DacPac.ps1
+++ src/Cmdlets/Public/Export-DacPac.ps1
@@ -1,7 +1,7 @@
 function Export-DacPac {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
         [string]
         $Path,
 
@@ -24,40 +24,42 @@ function Export-DacPac {
         $Overwrite
     )
 
-    $ErrorActionPreference = 'Stop'
+    process {
+        $ErrorActionPreference = 'Stop'
 
-    $Path = Normalize-Path($Path)
+        $Path = Normalize-Path($Path)
 
-    if ((Test-Path $Path) -and !$Overwrite) {
-        throw "File already exists."
-    }
-
-    $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
-
-    if ($PSCmdlet.ParameterSetName -eq 'ConnectionString') {
-        # HACK: For some reason setting the 'ConnectionString' property directly
-        # results in an ArgumentException:
-        #
-        # Keyword not supported: 'ConnectionString'
-        #
-        # Found this workaround somewhere on the internet.
-        $connectionStringBuilder.set_ConnectionString($ConnectionString)
-        
-        # If the DatabaseName parameter was passed set the Initial Catalog of the
-        # connection string builder to the database.
-        if (![string]::IsNullOrWhiteSpace($DatabaseName)) {
-            $connectionStringBuilder.set_InitialCatalog($DatabaseName)
+        if ((Test-Path $Path) -and !$Overwrite) {
+            throw "File already exists."
         }
-    }
-    else {
-        $connectionStringBuilder.Add("Data Source", $Server)
-        $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
-        $connectionStringBuilder.Add("User Id", $Credential.UserName)
-        $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
-    }
 
-    Write-Verbose "Exporting dacpac to $Path."
-    $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
-    $service.Extract($Path, $connectionStringBuilder.InitialCatalog, "SqlDevOps", [Version]::new(1,0,0,0))
-    Get-Item -Path $Path
+        $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
+
+        if ($PSCmdlet.ParameterSetName -eq 'ConnectionString') {
+            # HACK: For some reason setting the 'ConnectionString' property directly
+            # results in an ArgumentException:
+            #
+            # Keyword not supported: 'ConnectionString'
+            #
+            # Found this workaround somewhere on the internet.
+            $connectionStringBuilder.set_ConnectionString($ConnectionString)
+            
+            # If the DatabaseName parameter was passed set the Initial Catalog of the
+            # connection string builder to the database.
+            if (![string]::IsNullOrWhiteSpace($DatabaseName)) {
+                $connectionStringBuilder.set_InitialCatalog($DatabaseName)
+            }
+        }
+        else {
+            $connectionStringBuilder.Add("Data Source", $Server)
+            $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
+            $connectionStringBuilder.Add("User Id", $Credential.UserName)
+            $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
+        }
+
+        Write-Verbose "Exporting dacpac to $Path."
+        $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
+        $service.Extract($Path, $connectionStringBuilder.InitialCatalog, "SqlDevOps", [Version]::new(1,0,0,0))
+        Get-Item -Path $Path
+    }
 }
--- src/Cmdlets/Public/Publish-DacPac.ps1
+++ src/Cmdlets/Public/Publish-DacPac.ps1
@@ -1,64 +1,67 @@
 function Publish-DacPac {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true, Position=0)]
+        [Parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
         [string]
         $Path,
 
-        [Parameter(ParameterSetName='ConnectionString')]
+        [Parameter(ParameterSetName='ConnectionString', ValueFromPipelineByPropertyName=$true)]
         [string]
         $ConnectionString,
 
-        [Parameter(Mandatory=$true, ParameterSetName='NotConnectionString')]
+        [Parameter(Mandatory=$true, ParameterSetName='NotConnectionString', ValueFromPipelineByPropertyName=$true)]
         [string]
         $Server,
 
-        [Parameter(ParameterSetName='NotConnectionString')]
+        [Parameter(ParameterSetName='NotConnectionString', ValueFromPipelineByPropertyName=$true)]
         [PSCredential]
         $Credential,
 
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
         [string]
         $DatabaseName
     )
 
-    $ErrorActionPreference = 'Stop'
+    process {
+        $ErrorActionPreference = 'Stop'
 
-    if ($PSCmdlet.ParameterSetName -eq 'NotConnectionString' -and [string]::IsNullOrWhiteSpace($DatabaseName)) {
-        throw "The DatabaseName parameter is mandatory when authenticating with credentials."
-    }
-
-    # Normalize and validate the path.
-    $Path = Normalize-Path($Path)
-    if (!(Test-Path $Path)) {
-        throw "DacPac not found at: $Path"
-    }
-
-    $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
-
-    if ($PSCmdlet.ParameterSetName -eq 'ConnectionString') {
-        # HACK: For some reason setting the 'ConnectionString' property directly
-        # results in an ArgumentException:
-        #
-        # Keyword not supported: 'ConnectionString'
-        #
-        # Found this workaround somewhere on the internet.
-        $connectionStringBuilder.set_ConnectionString($connectionString)
-
-        # If the DatabaseName parameter was passed set the Initial Catalog of the
-        # connection string builder to the database.
-        if (![string]::IsNullOrWhiteSpace($DatabaseName)) {
-            $connectionStringBuilder.set_InitialCatalog($DatabaseName)
+        if ($PSCmdlet.ParameterSetName -eq 'NotConnectionString' -and [string]::IsNullOrWhiteSpace($DatabaseName)) {
+            throw "The DatabaseName parameter is mandatory when authenticating with credentials."
         }
+
+        # Normalize and validate the path.
+        $Path = Normalize-Path($Path)
+        if (!(Test-Path $Path)) {
+            throw "DacPac not found at: $Path"
+        }
+
+        $connectionStringBuilder = [System.Data.SqlClient.SqlConnectionStringBuilder]::new()
+
+        if ($PSCmdlet.ParameterSetName -eq 'ConnectionString') {
+            # HACK: For some reason setting the 'ConnectionString' property directly
+            # results in an ArgumentException:
+            #
+            # Keyword not supported: 'ConnectionString'
+            #
+            # Found this workaround somewhere on the internet.
+            $connectionStringBuilder.set_ConnectionString($connectionString)
+
+            # If the DatabaseName parameter was passed set the Initial Catalog of the
+            # connection string builder to the database.
+            if (![string]::IsNullOrWhiteSpace($DatabaseName)) {
+                $connectionStringBuilder.set_InitialCatalog($DatabaseName)
+            }
+        }
+        else {
+            $connectionStringBuilder.Add("Data Source", $Server)
+            $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
+            $connectionStringBuilder.Add("User Id", $Credential.UserName)
+            $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
+        }
+        
+        $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
+        $package = [Microsoft.SqlServer.Dac.DacPackage]::Load($Path)
+        $options = [Microsoft.SqlServer.Dac.PublishOptions]::new()
+        $service.Publish($package, $connectionStringBuilder.InitialCatalog, $options)
     }
-    else {
-        $connectionStringBuilder.Add("Data Source", $Server)
-        $connectionStringBuilder.Add("Initial Catalog", $DatabaseName)
-        $connectionStringBuilder.Add("User Id", $Credential.UserName)
-        $connectionStringBuilder.Add("Password", $Credential.GetNetworkCredential().Password)
-    }
-    
-    $service = [Microsoft.SqlServer.Dac.DacServices]::new($connectionStringBuilder.ConnectionString)
-    $package = [Microsoft.SqlServer.Dac.DacPackage]::Load($Path)
-    $options = [Microsoft.SqlServer.Dac.PublishOptions]::new()
-    $service.Publish($package, $connectionStringBuilder.InitialCatalog, $options)
 }
