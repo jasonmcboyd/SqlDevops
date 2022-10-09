0bd15202ab967c3f2f11e9bbbff49e7fd64ee848
Jason Boyd <jason.boyd.ce@gmail.com>
2022-10-09 13:44:53 -0700

    Removed old powershell module source code

--- src.powershell/Cmdlets/Private/Clone-TSqlModelOptions.ps1
+++
@@ -1,9 +0,0 @@
-# function Clone-TSqlModelOptions {
-#     param (
-#         [Microsoft.SqlServer.Dac.Model.TSqlModelOptions]
-#         $ModelOptions
-#     )
-
-#     $result = [Microsoft.SqlServer.Dac.Model.TSqlModelOptions]::new()
-
-# }
